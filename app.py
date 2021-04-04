3341fd4023726e5f5b247f18453ca6946ff8de33
Nicholas Padmanabhan <nicholaspad@gmail.com>
2021-04-04 15:20:57 -0400

    fixed wording

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def login():
     if not _db.is_user_created(netid):
         _db.create_user(netid)
 
-    print('user', netid, 'logged in')
+    print(f'user {netid} logged in')
 
     return redirect(url_for('dashboard'))
 
@@ -61,7 +61,7 @@ def dashboard():
 
     _db = Database()
     netid = _CAS.authenticate()
-    print('user', netid, 'logged in')
+    print(f'user {netid} used dashboard')
 
     data = _db.get_dashboard_data(netid)
     email = _db.get_user(netid)['email']
@@ -135,7 +135,7 @@ def get_course():
     courseid = request.args.get('courseid')
     query = request.args.get('query')
 
-    print('user', netid, 'accessed courseid', courseid)
+    print(f'user {netid} accessed courseid {courseid}')
 
     if query is None:
         query = ""
