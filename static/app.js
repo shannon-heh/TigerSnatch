4df84d0810e58691c1f9178a4944e7c370d70e6f
Shannon Heh <shannon.heh@princeton.edu>
2021-05-01 18:39:14 -0400

    changed wording, uploaded pfp

--- static/app.js
+++ static/app.js
@@ -1148,28 +1148,28 @@ let findMatches = function () {
         $.post(`/find_matches/${courseid}`, function (res) {
             // checks that user successfully updated section on back-end
             if (res["data"].length !== 0) {
-                s = `<div class="table-responsive">
+                s = `<div>
+                        <svg
+                            id="dev-warning"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-exclamation-triangle-fill text-primary"
+                            viewBox="0 0 16 18"
+                            >  
+                            <path
+                                d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+                            />
+                        </svg> Clicking 'Email' will add an entry for you & the match in Activity.
+                    </div>
+                    <div class="table-responsive">
                         <table class="table table-hover mt-2">
                         <thead class="table-white">
                             <tr>
                                 <th scope="col">NetID</th>
                                 <th scope="col">Current Section</th>
-                                <th scopt="col">Contact <svg
-                                    id="dev-warning"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    class="bi bi-exclamation-triangle-fill text-primary ms-1"
-                                    viewBox="0 0 16 16"
-                                    data-bs-toggle="tooltip"
-                                    data-bs-placement="top"
-                                    title="Clicking 'Email' will notify the user in their Activity page!"
-                                >
-                                    <path
-                                        d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-                                    />
-                                </svg></th>
+                                <th scopt="col">Contact</th>
                             </tr>
                         </thead>
                             <tbody>`;
